69d491ad4a40c7f22b58878b04b75f15f8ea3701
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-03-04 21:40:12 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,11 +1,17 @@
+#change to homework dir
 cd ~
 
+#download the zim file from the input url
 wget -qO wiki.zim $1
 
+#clean the wikipedia dir
 rm -rf ./docs
 
+#dump the zim file to html/js/css files etc.
 ./zim-tools_linux-x86_64-3.1.0/zimdump dump --dir=./docs --ns A wiki.zim
 
+#clean the zim file
 rm -f wiki.zim
 
+#run the script to mirror the wikipedia files to swarm and output the index.html file hash content on the swarm
 python3 /mirror.py
