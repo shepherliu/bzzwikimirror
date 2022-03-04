de53bc4694b36f7e48e89e997e28ffb2cf221ca5
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-03-04 22:05:27 +0800

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -3,16 +3,16 @@
 #!/usr/bin/python3
 import os
 
-wikipediaDir="~/docs"
+wikipediaDir = os.path.join(os.environ['HOME'], 'docs')
 
-htmlDir="~/docs/A"
+htmlDir = os.path.join(os.environ['HOME'], 'docs/A')
 
 def getFiles(path):
   items = os.listdir(path)
   files = []
   
   for item in items:
-    item = os.path.join(path,item)
+    item = os.path.join(path, item)
     if os.path.isdir(item):
       files.extend(getFiles(item))
     elif os.path.isfile(item):
