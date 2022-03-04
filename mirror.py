d84c79c1a20961e5d3e6b32b3a749bfcd37f467c
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-03-04 21:50:44 +0800

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -2,4 +2,19 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
 
-print ("Hello, Python!")
+wikipediaDir="~/docs"
+
+htmlDir="~/docs/A"
+
+def getFiles(path):
+  items = os.listdir(path)
+  files = []
+  
+  for item in items:
+    item = os.path.join(path,item)
+    if os.path.isdir(item):
+      files.append(getFiles(item))
+    elif os.path.isfile(item):
+      files.append(item)
+  
+  return files    
