1da91d0f8754f09b83b6ca89689f857bd77683ef
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-03-05 15:32:55 +0800

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -173,7 +173,7 @@ def uploadToSwarm(filepath):
 if __name__ == '__main__':
   for file in getFiles(wikipediaDir):
     result = uploadToSwarm(file) 
-    return
+    break
                                      
                                      
                                      
