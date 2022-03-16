37df10487177d4b637d04e6bf693bdfc75e4ae1a
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-03-16 17:22:05 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -13,12 +13,8 @@ rm -rf ./docs
 #clean the zim file
 rm -f wiki.zim
 
-#move index file to root path and rename to html type
-mv docs/A/index docs/index.html
-
-#modify the index.html to redirect to the correct url
-sed -i 's#url=#url=A/#g' docs/index.html
-sed -i 's#href=\"#href=\"A/#g' docs/index.html
+#create a new index.html instead
+rm -f docs/A/index
 
 #fix python3 http client encode to utf-8
 sed -i 's#latin-1#utf-8#g' /usr/lib/python3.8/http/client.py
