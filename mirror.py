23ba741b33e94932715abc4946e9903868245b15
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-03-16 17:20:52 +0800

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -56,6 +56,19 @@ headList = ['fileName', 'fileMode', 'uid', 'gid', 'fileSize', 'mtime', 'checksum
 #using to patch content length
 headPatch = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
+#create index.html
+def createIndexHtml(files):
+  html = '''<html><head><meta charset="UTF-8"><title>Wikipedia</title><meta name="viewport" content="width=device-width, initial-scale=1.0"><script>function importScript() { return 1 } // this is to avoid the error from site.js</script><script src="./-/masonry.min.js"></script><script src="./-/script.js"></script><link href="./-/mobile_main_page.css" rel="stylesheet" type="text/css"><link href="./-/style.css" rel="stylesheet" type="text/css"><script src="./-/images_loaded.min.js"></script><script src="./-/masonry.min.js"></script><script src="./-/article_list_home.js"></script><link rel="canonical" href="https://en.wikipedia.org/wiki/"></head><body class="article-list-home mw-body-content nojs"><ul id="list">'''
+  
+  for file in files:
+    if file.startswith('./A/'):
+      filename = os.path.basename(file)
+      html = html + '<li style="width:100%; height: auto; word-wrap:break-word; word-break:break-all;"><a></a><a href="A/{0}">{1}</a><a></a></li><a></a>'.format(filename, filename)
+
+  html = html + '</ul></body></html>'
+
+  return html.encode('utf-8')
+
 #pad number to fix length in oct
 def pad(num, length):
   bits = oct(num)[2:]
@@ -99,7 +112,7 @@ def addHeader(filename, length):
   #caculate the checksum
   checksum = addChecksum(fileHeader)
   
-  #pad the checksum	
+  #pad the checksum 
   fileHeader['checksum'] = pad(checksum, 6) + '\u0000 '
   
   buffer = b''
@@ -173,14 +186,19 @@ def collectFilesData(files):
   data = b''
   sum = 0
 
-  #we use the website type as default to upload files to swarm	
+  #we use the website type as default to upload files to swarm  
   metaName = 'website'
 
   if len(files) == 0:
     return None
   elif len(files) == 1:
-    #the metaName is the file name itself when upload a sigle file		
+    #the metaName is the file name itself when upload a sigle file    
     metaName = os.path.basename(files[0])
+
+  #add index.html first 
+  indexHtml = createIndexHtml(files)
+  sum = sum + len(indexHtml)
+  data = appendTarFile(data, 'index.html', indexHtml)
 
   for file in files:
     content = open(file, 'rb').read()
@@ -196,7 +214,7 @@ def collectFilesData(files):
     if len(data) + len(content) > maxUploadSize:
       break
     
-    #for a sigle file, we use the base filename, for multi files we use relative path	
+    #for a sigle file, we use the base filename, for multi files we use relative path 
     if len(files) == 1:
       file = os.path.basename(file)
     else:
@@ -208,7 +226,7 @@ def collectFilesData(files):
   #get metafile info
   metafile, metadata = addMetaFile(metaName, sum)
 
-  #add meta file info to the collection	
+  #add meta file info to the collection 
   data = appendTarFile(data, metafile, metadata)
 
   #make sure the data is not less than 20 blocks, each block is 512 bytes
@@ -239,7 +257,7 @@ def uploadToSwarm(files):
             }
 
   reference = ""
-  #try to all swarm gateways to upload	
+  #try to all swarm gateways to upload  
   for swarmUrl in swarmGateways:
     try:
       r = requests.post(swarmUrl, data = data, headers = headers)
@@ -247,33 +265,33 @@ def uploadToSwarm(files):
       if r.status_code < 200 or r.status_code > 299:
         reference = r.text
         continue
-	
+  
       reference = json.loads(r.text).get('reference')
       return reference
     except:
-      continue	
+      continue  
   
   return reference
 
 if __name__ == '__main__':
-  # use your own swarm gateway instead	
+  # use your own swarm gateway instead  
   if len(sys.argv) > 1 and len(sys.argv[1]) > 0:
     swarmGateways = [sys.argv[1]]
     # if use your own swarm gateway and bee node, the upload size can be large by your own settings, so set it bigger
     maxUploadSize = 1024*1024*1024
-  # use your own swarm gateway batch id instead	
+  # use your own swarm gateway batch id instead 
   if len(sys.argv) > 2 and len(sys.argv[2]) > 0:
-    swarmPostageBatchId = sys.argv[2]  		
-  #change to wiki docs dir	
+    swarmPostageBatchId = sys.argv[2]     
+  #change to wiki docs dir  
   os.chdir(wikipediaDir)
   
   #files need to upload to swarm
   #for now swarm gateway has a limit of max 10M to upload, so we need to select some importent files to upload
-  files = ["./index.html"]
-	
-  #dir A stores html files, and dir - stores js,css files	
+  files = []
+  
+  #dir A stores html files, and dir - stores js,css files 
   for file in getFiles('.'):
-    #swarm gateway has a limit file name to less than 100bytes		
+    #swarm gateway has a limit file name to less than 100bytes    
     if len(file.encode(encoding = 'utf-8')) - 2 > headFormat['fileName']:
       continue
     
@@ -289,7 +307,7 @@ if __name__ == '__main__':
       files.append(file)
 
   # upload files to swarm
-  result = uploadToSwarm(files)	
+  result = uploadToSwarm(files) 
   
   #print the 'reference' of the files stored in swarm
   #we can visit the website on https://gateway-proxy-bee-8-0.gateway.ethswarm.org/bzz/reference, remember replace the real reference
