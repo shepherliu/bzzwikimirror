8e4031117a47e13344e3b56a1e6f47a96f7a6a50
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-06-10 15:14:25 +0800

    add http server

---
+++ httpserver/httpserver.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3 
+# -*- coding: utf-8 -*-
+import os
+import sys
+import json
+import time
+import re
+import getopt
+import enum
+import logging
+import mimetypes
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from fairos.fairos import Fairos
+
+FAIROS_HOST = "https://fairos.fairdatasociety.org"
+
+FAIROS_VERSION = 'v1'
+
+POD_NAME = 'wikimedia_zim'
+
+TABLE_ZIM = 'zim_status'
+
+TABLE_FILE = 'file_status'
+
+TABLE_INDEX = 'index_status'
+
+fs = None
+root = '~/dist'
+
+logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', level=logging.INFO)
+
+class Resquest(BaseHTTPRequestHandler):
+	def do_GET(self):
+		global root
+
+		path = self.path.split('?')[0]
+
+		if path == '/':
+			path = '/index.html'
+
+		localpath = os.path.join(root, path.split('/')[1])
+
+		types, encoding = mimetypes.guess_type(path)
+		if types is None:
+			types = 'text/plain'
+
+		if os.path.isfile(localpath):
+			return self.getFileFromLocal(localpath, types)
+		else:
+			return self.getFileFromFairOs(path, types)
+
+	#read file from local system
+	def getFileFromLocal(self, filepath:str, types:str):
+		try:
+			content = open(filepath, 'rb').read()
+			self.send_response(200)
+			self.send_header('Content-type', types)
+			self.end_headers()
+			self.wfile.write(content)			
+		except:
+			return self.notFoundPage()
+
+	#read file from fair os
+	def getFileFromFairOs(self, filepath:str, types:str):
+		global fs
+
+		try:
+			res = fs.download_file(POD_NAME, filepath)
+			if res['message'] != 'success':
+				#sync pod and try again
+				fs.sync_pod(POD_NAME)
+				res = fs.download_file(POD_NAME, filepath)
+
+			if res['message'] != 'success':
+				logging.error(f"read {filepath} from fairos error: {res['message']}")
+				return self.notFoundPage()
+			
+			self.send_response(200)
+			self.send_header('Content-type', types)
+			self.end_headers()
+			self.wfile.write(res['content'])
+		except:
+			return self.notFoundPage()
+
+	#not found page
+	def notFoundPage(self):
+		self.send_response(404)
+		self.send_header('Content-type', 'text/html')	
+		self.end_headers()
+		self.wfile.write(f"<html><body><h4>file is not found</h4></body></html>".encode())		
+
+#init fairos module
+def init_fairos(username, password, host = FAIROS_HOST, version = FAIROS_VERSION, podname = POD_NAME):
+
+	fs = Fairos(host, version)
+
+	#login user
+	res = fs.login_user(username, password)
+
+	if res['message'] != 'success':
+		logging.error(f"login user: {username} error: {ret['message']}")
+		return None
+	else:
+		logging.info(f"login user: {username} success")
+
+	#open pod
+	res = fs.open_pod(podname)
+
+	if res['message'] != 'success':
+		logging.error(f"open pod: {podname} error: {ret['message']}")
+		return None
+	else:
+		logging.info(f"open pod: {podname} success")
+	
+	return fs
+
+if __name__ == '__main__':
+	argv = sys.argv[1:]
+
+	host = FAIROS_HOST
+	version = FAIROS_VERSION
+	user = ''
+	password = ''
+	root = '~/dist'
+
+	try:
+		opts, args = getopt.getopt(argv, "h:v:u:p:d:", [
+			"host=",
+			"version=",
+            "user=",
+            "password=",
+            "root="
+        ])
+	except:
+		logging.error("parse arguments failed")
+		sys.exit(-1)
+
+	for opt, arg in opts:
+		if opt in ['--host','-h']:
+			host = arg
+		elif opt in ['--version','-v']:
+			version = arg
+		elif opt in ['--user','-u']:
+			user = arg
+		elif opt in ['--password','-p']:
+			password = arg
+		elif opt in ['--root', '-r']:
+			root = arg
+
+	if not os.path.exists(root):
+		logging.error(f"root path: {root} not exists")
+		sys.exit(-1)
+
+	fs = init_fairos(user, password, host, version)
+	if fs is None:
+		sys.exit(-1)
+
+	server = HTTPServer(('0.0.0.0', 8080), Resquest)
+	server.serve_forever()
